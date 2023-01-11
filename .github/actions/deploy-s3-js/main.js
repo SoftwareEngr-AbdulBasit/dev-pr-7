8180406ef68fa275db2b9af85562270604ad709d
Abdul Basit <basit9498@gmail.com>
2023-01-11 13:38:48 +0500

    main custom JS  action update

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -3,7 +3,17 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
-  core.notice("Helow my js custom action");
+  // get input value
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const distFolder = core.getInput(" dist-folder", { required: true });
+
+  // upload the file to AWS S3
+  const s3Url = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Url} --region ${bucketRegion}`);
+
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput("website-url", websiteUrl);
 }
 
 run();
